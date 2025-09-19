51f96354415b15f743c4e5cfc1cb5828250428d7
Zacksly <65759438+Zacksly@users.noreply.github.com>
2025-09-19 14:13:13 -0400

    Test Version 1.0

--- kb.js
+++ kb.js
@@ -1,3 +1,5 @@
+// Service worker for Manifest V3
 browser.webNavigation.onCompleted.addListener(function (details) {
-    browser.tabs.sendMessage(details.tabId,JSON.stringify({"directive":"insertKeyboard"}));
+    browser.tabs.sendMessage(details.tabId, JSON.stringify({"directive":"insertKeyboard"}))
+        .catch(error => console.error("Error sending message to content script:", error));
 });
